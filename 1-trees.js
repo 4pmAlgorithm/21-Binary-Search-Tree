cfd8d340c49195be20d0a0656a37d04d13116ed5
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-24 14:34:07 -0400

    binary search tree

---
+++ 1-trees.js
@@ -0,0 +1,12 @@
+//parent-child relationship
+
+//everything is pointing to parent. Not from the same parent level to another parent level. only up and down.
+
+//root - the top node in a tree
+//child - a node directly connected to another node when moving away from the root
+//parent - the converse notion of a child
+//siblings - a group of nodes with the same parent
+//leaf - a node with no children
+//edge - the connection between one node and another
+
+//ex) HTML DOM, networking routing, 
---
+++ 2-binarySearchTrees.js
@@ -0,0 +1,4 @@
+// binary search tree BST
+//1. children nodes are always 2 or less
+//2. left child is less than the parent node.
+//3. child on the right is always greater than the parent.
